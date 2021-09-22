0e3c82b562d0f24aa277a8c4baac3c6cb5adc0cf
Mehak Zehra <mehak.rizvi.786@gmail.com>
2021-09-22 02:38:36 -0700

    Added query parameter category

--- controllers/recipes-routes.js
+++ controllers/recipes-routes.js
@@ -5,47 +5,22 @@ const path = require('path');
 const { recipes } = require("../db/db.json");
 
 
-// get /api/notes check db.json and returns all saved notes as json
-router.get("/recipes", (req,res) => {
+// api/recipes/?search_query=burgers
+// api/recipes/?id=23456543
+// api/recipes/?category=lunch
+router.get("/recipes/", (req,res) => {
+  // get query parameters from req.headers
+  console.log(req.query.category);
+
+  // if query.id is not null then call function getRecipeById(query.id) ==> array of recipes
+  // if search query is not null then call function searchRecipesByQuery(string)
+  // category
   console.log("Hello recipes!")
   res.json(recipes);
 })
-// post /api/notes receive a new note to save on the request and save to db.json then return new note to client
-// router.post("/notes", (req, res) => {
-//   // set id based on what the next index of the array will be
-//   req.body.id = uniqid();
-//   // if any data if invalid, send 400 error
-//   if(!req.body.title || !req.body.text){
-//     res.status(400).send('The note is not valid!')
-//   } else {
-//     const note = createNewNote(req.body);
-//     res.json(note);
-//   }
-// });
 
-// router.delete('/notes/:id', (req, res) => {
-//   let id = req.params.id
-//   for( var i = 0; i < notes.length; i++){
-//     if(notes[i].id === id){
-//       notes.splice(i, 1)
-//       fs.writeFileSync(
-//         path.join(__dirname, '../../db/db.json'),
-//         JSON.stringify({ notes : notes}, null, 2)
-//       );
-//     }
-//   }
-//   return res.json(notes);
-// })
 
-// create new note function
-function createNewNote(body) {
-  const note = body;
-  notes.push(note);
-  fs.writeFileSync(
-    path.join(__dirname, '../../db/db.json'),
-    JSON.stringify({ notes : notes}, null, 2)
-  );
-  return notes;
-}
+
+
 
 module.exports = router;
--- views/homepage.js
+++ views/homepage.js
@@ -6,10 +6,6 @@ router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
   });
   
-//   router.get('/notes', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../../public/notes.html'));
-//   });
-  
   //Wildcard route: The * will act as a wildcard, meaning any route that wasn't previously defined will fall under this request and will receive the homepage as the response.
   router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
