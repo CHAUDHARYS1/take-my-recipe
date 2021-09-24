1726ec7547ccda23809dc672024d0bf4579e4f4f
Jaya <jaya.vedantam@gmail.com>
2021-09-23 18:36:40 -0700

    adding login route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,38 +3,45 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Recipe, User, Comment } = require('../models');
 
+
 //Home page route 
-
-
 router.get('/', (req, res) => {
   console.log("home route called")
- /*   console.log(req.session)
-    console.log("home route called")
+   console.log(req.session)
+ 
     Recipe.findAll({
          attributes: [
             'id',
             'title',
             'description',         
             'category',
-            'ingredients'
+            'ingredients',
+            'imageUrl'
         ],
     })
         .then(dbPostData => {
           const recipes = dbPostData.map(recipe => recipe.get({ plain: true }));
           console.log("recipes", recipes);
-          res.render('index', {
-            recipes
-           // loggedIn: req.session.loggedIn
+
+          res.render('allRecipes', {            
+          recipes,
+          loggedIn: req.session.loggedIn
           });
         })
         .catch(err => {
           console.log(err);
           res.status(500).json(err);
-        }); */
-        res.render('index');
+        }); 
 }); 
 
+//login / signup page
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
 
-
+  res.render('login');
+});
 
 module.exports = router;
