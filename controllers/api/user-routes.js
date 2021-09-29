7a6dd104ab45594b9c9da34c77f080f4368d553e
Jaya <jaya.vedantam@gmail.com>
2021-09-29 15:04:13 -0700

    latest changes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -46,7 +46,7 @@ router.post('/login', (req, res) => {
         }
   }).then(dbUserData => {
     if (!dbUserData) {
-      res.status(400).json({ message: 'No user with that username!' });
+      res.status(400).json({ message: 'No user with that email!' });
       return;
     }
 
