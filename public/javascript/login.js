0d15ef2cabd7d128295d70f88288353090f4cd94
Shital Chaudhary <shitalchau10@gmail.com>
2021-10-02 11:17:34 -0500

    add ability to see/unsee password, issue closed

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,29 +1,42 @@
-
 async function loginFormHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const email = document.querySelector('#email').value.trim();
-    const password = document.querySelector('#password').value.trim();
-  
-    if (email && password) {
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert(response.statusText);
+  const email = document.querySelector('#email').value.trim();
+  const password = document.querySelector('#password').value.trim();
+
+  if (email && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({
+        email,
+        password
+      }),
+      headers: {
+        'Content-Type': 'application/json'
       }
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
     }
   }
+}
 
-  //var loginBtn = document.getElementById('login');
-  var loginBtn = document.querySelector('#login');
-  
-  loginBtn.addEventListener("click", loginFormHandler);
+//var loginBtn = document.getElementById('login');
+var loginBtn = document.querySelector('#login');
+
+loginBtn.addEventListener("click", loginFormHandler);
+
+
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#password');
+
+togglePassword.addEventListener('click', function (e) {
+  // toggle the type attribute
+  const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+  password.setAttribute('type', type);
+  // toggle the eye / eye slash icon
+  this.classList.toggle('bi-eye');
+});
