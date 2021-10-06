ca6ff8c93dc1c5b0b80929958efe35d2cd7e4aa5
Shital Chaudhary <shitalchau10@gmail.com>
2021-10-05 23:01:27 -0500

    add form validation for sign up

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,14 +1,48 @@
-function signup(event) {
+async function signup(event) {
+
   event.preventDefault();
   const email = document.getElementById("emailSU").value;
   const firstName = document.getElementById("firstName").value;
   const lastName = document.getElementById("lastName").value;
   const password = document.getElementById("pwSU").value;
   const confirmPassword = document.getElementById("pwConfirmSU").value;
-  //Password check
-  if (password != confirmPassword) {
-    alert("Password does not match");
+  const errorMessage = document.querySelector('#error');
+  const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  if (firstName === !null) {
+
+  } else if (firstName === null || firstName === "") {
+    errorMessage.classList.remove("uk-hidden");
+    errorMessage.innerText = "Please enter a first name";
     return false;
+  } else if (lastName === null || lastName === "") {
+    errorMessage.classList.remove("uk-hidden");
+    errorMessage.innerText = "Please enter a last name";
+    return false;
+  } else if (email === null || email === "") {
+    errorMessage.classList.remove("uk-hidden");
+    errorMessage.innerText = "Please enter a email address";
+    return false;
+  } else if(password === null || password === ""){
+    errorMessage.classList.remove("uk-hidden");
+    errorMessage.innerText = "Please enter a password";
+    return false;
+  } else if(confirmPassword === null || confirmPassword === ""){
+    errorMessage.classList.remove("uk-hidden");
+    errorMessage.innerText = "Please enter confirm password";
+    return false;
+  } else if(password !== confirmPassword){
+    errorMessage.classList.remove("uk-hidden");
+    errorMessage.innerText = "Password does not match";
+    return false;
+  }
+      else if (email.match(!validRegex)) {
+        errorMessage.classList.add("uk-hidden");
+        errorMessage.innerText = "Please enter a correct email address";
+        return false;
+      }
+   else {
+
   }
   fetch("/api/users/", {
     method: "post",
@@ -18,7 +52,9 @@ function signup(event) {
       email,
       password,
     }),
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json"
+    },
   }).then((response) => {
     if (response.ok) {
       document.location.replace("/login");
