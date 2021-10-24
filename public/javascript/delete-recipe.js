c27a7314f1a6ef3555764a92f083c8744dc681dc
Jaya <jaya.vedantam@gmail.com>
2021-10-23 17:58:29 -0700

    fix bug 45, ability to edit current data on edit recipe page

--- public/javascript/delete-recipe.js
+++ public/javascript/delete-recipe.js
@@ -1,14 +1,11 @@
 async function deleteFormHandler(event) {
    event.preventDefault();
-      
-   /*  event.preventDefault();
-    const title = document.getElementById('recipe-title').value;
-    const category = document.getElementById('form-stacked-select').value;
-    const description = document.getElementById('description').value;
-    const instructions = document.getElementById('instructions').value;
-    const imageUrl = document.getElementById('image_link').value; 
-    const ingredients = document.getElementById('ingredients').value;*/
-  
+   
+   var confirmDelete = window.confirm(
+    "Are you sure you want to delete this recipe? "
+  );
+
+   if(confirmDelete) {
     const id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 1
     ];
@@ -17,11 +14,12 @@ async function deleteFormHandler(event) {
         method: 'DELETE'
       });    
       if (response.ok) {
-        document.location.replace('/dashboard/');
+        document.location.replace('/dashboard');
       } else {
         alert(response.statusText);
       }
     }
+  }
     
     var deleteBtn = document.querySelector('#delete-btn');
     deleteBtn.addEventListener("click", deleteFormHandler);
